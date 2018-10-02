f7c24ab14a5066ec10f8f07c8333b2aa8a2aed8c
Benfarhat <benfarhat.elyes@gmail.com>
2018-10-02 08:28:43 +0100

    animate.css

--- application/views/elements/header.php
+++ application/views/elements/header.php
@@ -3,18 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!doctype html>
 <html lang="en">
   <head>
+    <title><?= $title ?? '' ?></title>
     <!-- Required meta tags -->
 	<?= meta("UTF-8", "", 'charset') ?>
 	<?= meta("X-UA-Compatible", "IE=edge", 'http-equiv') ?>
-	<?= meta("viewport", "width=device-width, initial-scale=1") ?>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <?= meta("viewport", "width=device-width, initial-scale=1, shrink-to-fit=no") ?>
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
 	<!-- Font Awesome -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title><?= $title ?? '' ?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" />
   </head>
   <body>
 
